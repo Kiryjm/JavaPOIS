a2e5eeda308e688f00298b0ad3f9cb5acd034905
Kiryjm <kiryjm@gmail.com>
2018-05-06 03:47:58 +0200

    Lab4

---
+++ Lab4/src/Task_A/Lab04A.java
@@ -0,0 +1,24 @@
+package Task_A;
+
+/*
+ Задание А. Разработайте программу, которая проверяет, что точка с координатами (x, y)
+лежит внутри прямоугольника, левая верхняя вершина которого имеет коор-
+динаты (x1, y1), правая нижняя – (x2, y2), а стороны параллельны координат-
+ным осям.
+ */
+
+public class Lab04A {
+
+    public static void main(String[] args) {
+        
+                // Задание координат левого нижнего 
+        double upperLeftAngleX = -14.5;
+        double upperLeftAngleY = 101.2;
+                // и правого верхнего углов прямоугольника
+        double lowerRightAngleX = 9.3;
+        double lowerRightAngleY = 102.7;
+        
+        System.out.println("");
+    }
+    
+}
