431923b4415b554628137811171efdc715fe84cd
Kiryjm <kiryjm@gmail.com>
2018-05-06 04:02:29 +0200

    Lab4

--- Lab4/src/Task_A/Lab04A.java
+++ Lab4/src/Task_A/Lab04A.java
@@ -1,8 +1,8 @@
 package Task_A;
 
 /*
- Задание А. Разработайте программу, которая проверяет, что точка с координатами (x, y)
-лежит внутри прямоугольника, левая верхняя вершина которого имеет коор-
+ Задание А. Разработайте программу, которая проверяет, что точка с координатами 
+(x, y)лежит внутри прямоугольника, левая верхняя вершина которого имеет коор-
 динаты (x1, y1), правая нижняя – (x2, y2), а стороны параллельны координат-
 ным осям.
  */
@@ -11,14 +11,20 @@ public class Lab04A {
 
     public static void main(String[] args) {
         
-                // Задание координат левого нижнего 
         double upperLeftAngleX = -14.5;
         double upperLeftAngleY = 101.2;
-                // и правого верхнего углов прямоугольника
         double lowerRightAngleX = 9.3;
-        double lowerRightAngleY = 102.7;
+        double lowerRightAngleY = 1.7;
+        double pointX = -10.48;
+        double pointY = 25.85;
         
-        System.out.println("");
+        System.out.println("Check, if point with coordinates (" 
+                + pointX + ", " + pointY +")");
+        System.out.println("lying into the rectangle");
+        System.out.println("Result: " + 
+                Logic.inRectangle(pointX, pointY, 
+                upperLeftAngleX, upperLeftAngleY, 
+                lowerRightAngleX, lowerRightAngleY));
     }
     
 }
